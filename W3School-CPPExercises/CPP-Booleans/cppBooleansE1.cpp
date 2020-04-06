2250ede707c9fc3425315e339a29340b36cc012e
Alexoah YT <alexandra.soah@gmail.com>
2020-04-06 13:29:58 +0700

    added answer files for C++ Booleans exercises

---
+++ W3School-CPPExercises/CPP-Booleans/cppBooleansE1.cpp
@@ -0,0 +1,16 @@
+/**
+ *	from C++ Booleans: Exercise 1 ( https://www.w3schools.com/cpp/exercise.asp?filename=exercise_booleans1 )
+ *
+ *	question:
+ *	Fill in the missing parts to print the values 1 (for true) and 0 (for false):
+ *
+ *	____ isCodingFun = true;
+ *	____ isFishTasty = false;
+ *	cout << ___________;
+ *	cout << ___________;
+ *
+ */
+bool isCodingFun = true;
+bool isFishTasty = false;
+cout << isCodingFun;
+cout << isFishTasty;
---
+++ W3School-CPPExercises/CPP-Booleans/cppBooleansE2.cpp
@@ -0,0 +1,14 @@
+/**
+ *	from C++ Booleans: Exercise 2 ( https://www.w3schools.com/cpp/exercise.asp?filename=exercise_booleans2 )
+ *
+ *	question:
+ *	Fill in the missing parts to print the value true (for true):
+ *
+ *	int x = 10;
+ *	int y = 9;
+ *	cout << (_ _ _);
+ *
+ */
+int x = 10;
+int y = 9;
+cout << (x > y);
